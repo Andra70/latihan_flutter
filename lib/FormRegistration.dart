cefa9a864aba521ee79f48236947bb4276f1e13a
Marshlaw <abhinayaandra@gmail.com>
2022-08-01 11:20:28 +0700

    Feat [AAS] phone number

--- lib/FormRegistration.dart
+++ lib/FormRegistration.dart
@@ -21,81 +21,84 @@ class _FormRegistrationState extends State<FormRegistration> {
       appBar: AppBar(
         title: Text("Formregistration"),
       ),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Username',
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                ),
               ),
-            ),
-            Container(
-              height: 20,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
+              Container(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Pilih jenis kelamin",
-              style: TextStyle(fontSize: 15),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(children: [
-              Radio(
-                  value: 1,
-                  groupValue: id,
-                  onChanged: (val) {
-                    setState(() {
-                      id = 1;
-                    });
-                  }),
-              Text("Male")
-            ]),
-            Row(
-              children: [
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Pilih jenis kelamin",
+                style: TextStyle(fontSize: 15),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(children: [
                 Radio(
-                    value: 2,
+                    value: 1,
                     groupValue: id,
                     onChanged: (val) {
                       setState(() {
-                        id = 2;
+                        id = 1;
                       });
                     }),
-                Text("female")
-              ],
-            ),
-            Text("Phone number : "+myPhone),
-            ElevatedButton(
-                onPressed: () async {
-                  final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SecondRoute()),
-                  );
-                  print("Hasil input phone number " + result.toString());
-                  setState(() {
-                    myPhone = result.toString();
-                  });
-                },
-                child: Text("Input phone number")),
-            Center(
-                child: Container(
-              width: 200,
-              height: 50,
-              child: ElevatedButton(onPressed: () {}, child: Text("Submit")),
-            ))
-          ],
+                Text("Male")
+              ]),
+              Row(
+                children: [
+                  Radio(
+                      value: 2,
+                      groupValue: id,
+                      onChanged: (val) {
+                        setState(() {
+                          id = 2;
+                        });
+                      }),
+                  Text("female")
+                ],
+              ),
+              Text("Phone number : "+myPhone),
+              ElevatedButton(
+                  onPressed: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SecondRoute()),
+                    );
+                    print("Hasil input phone number " + result.toString());
+                    setState(() {
+                      myPhone = result.toString();
+                    });
+                  },
+                  child: Text("Input phone number")),
+              Center(
+                  child: Container(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(onPressed: () {}, child: Text("Submit")),
+              ))
+            ],
+          ),
         ),
       ),
     );
