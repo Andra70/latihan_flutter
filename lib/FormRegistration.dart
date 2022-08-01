3dc90175977eb9549311964159dc1deba6857eda
Marshlaw <abhinayaandra@gmail.com>
2022-08-01 10:41:58 +0700

    phone number

--- lib/FormRegistration.dart
+++ lib/FormRegistration.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'SecondRoute.dart';
+
 class FormRegistration extends StatefulWidget {
   FormRegistration({Key? key}) : super(key: key);
 
@@ -11,69 +13,88 @@ class _FormRegistrationState extends State<FormRegistration> {
   TextEditingController ctrUsername = new TextEditingController();
   TextEditingController ctrPassword = new TextEditingController();
   int id = 1;
+  String myPhone= "-";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Formregistration"),),
+      appBar: AppBar(
+        title: Text("Formregistration"),
+      ),
       body: Container(
         margin: EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextField(
-
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
-              labelText: 'Username',
+                labelText: 'Username',
               ),
             ),
-            Container(height: 20,),
+            Container(
+              height: 20,
+            ),
             TextField(
-
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
-              labelText: 'Password',
+                labelText: 'Password',
               ),
             ),
-            SizedBox(height: 20,),
-            Text("Pilih jenis kelamin", style: TextStyle(fontSize: 15),),
-            SizedBox(height: 10,),
-            Row (
-              children: [
-                Radio(
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Pilih jenis kelamin",
+              style: TextStyle(fontSize: 15),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(children: [
+              Radio(
                   value: 1,
                   groupValue: id,
                   onChanged: () {
                     setState(() {
-                      id = 1;  
-                      });
-                  }()
-                ),
-                Text("Male")
-              ]
-            ),
-             Row (
+                      id = 1;
+                    });
+                  }()),
+              Text("Male")
+            ]),
+            Row(
               children: [
                 Radio(
-                  value: 2,
-                  groupValue: id,
-                  onChanged: () {
-                    setState(() {
-                      id = 2; 
-                    });
-                  }()
-                ),
+                    value: 2,
+                    groupValue: id,
+                    onChanged: () {
+                      setState(() {
+                        id = 2;
+                      });
+                    }()),
                 Text("female")
               ],
             ),
-            Center (
-              child: Container(
-                width: 20,
-                child: ElevatedButton(onPressed: () {},
-                    child: Text("Submit")),
-              )
-            )
+            Text("Phone : "+myPhone),
+            ElevatedButton(
+                onPressed: () async {
+                  final result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SecondRoute()),
+                  );
+                  print("Hasil input phone number " + result.toString());
+                  setState(() {
+                    myPhone = result.toString();
+                  });
+                },
+                child: Text("Input phone number")),
+            Center(
+                child: Container(
+              width: 200,
+              height: 50,
+              child: ElevatedButton(onPressed: () {}, child: Text("Submit")),
+            ))
           ],
         ),
       ),
---
+++ lib/SecondRoute.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatefulWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  State<SecondRoute> createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  TextEditingController ctrPhone = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextField(
+                controller: ctrPhone,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Phone number',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, ctrPhone.text);
+                },
+                child: Text("Submit"),
+              )
+            ],
+          )),
+    );
+  }
+}
